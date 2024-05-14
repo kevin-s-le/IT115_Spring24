dad1cbff9745dc8334926c2486d7a7a012bfa448
kevin-s-le <kevin.s.le@seattlecolleges.edu>
2024-05-14 09:38:56 -0700

    first commit for IT 115

---
+++ json830Class.py
@@ -0,0 +1,21 @@
+#
+import json
+
+data = {
+    
+    'name': 'John Doe',
+    'age': 30,
+    'city': 'New York, NY',
+    'interests': ['Golf', 'Running', 'Football', 'Traveling']
+    'is_student': False
+    
+}
+
+echo "# IT115_Spring24" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kevin-s-le/IT115_Spring24.git
+git push -u origin main
+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
